8909001730f1158fea4547f533c5a0522955b2b7
M2U7BF <yhvh4321@gmail.com>
2025-05-19 17:58:26 +0900

    update

--- test.sh
+++ test.sh
@@ -5,6 +5,11 @@ use_func_test=1
 arg_test=1
 main_test=1
 
+check_exit_status()
+{
+  if [ $? -ne $1 ]; then echo NG; else echo OK; fi
+}
+
 make re
 if [ $? -ne 0 ]; then
   exit 1
@@ -14,52 +19,59 @@ echo ""
 if [ $use_func_test -eq 1 ]; then
   echo "使用関数のチェック -------------------------------------------"
   nm -u ./philo | grep GLIBC | grep -v -E '__libc_start_main|memset|printf|malloc|free|write|usleep|gettimeofday|pthread_create|pthread_detach|pthread_join|pthread_mutex_init|pthread_mutex_destroy|pthread_mutex_lock|pthread_mutex_unlock'
-  if [ $? -eq 1 ]; then
-    echo "OK"
-  fi
+  check_exit_status 1
   echo ""
 fi
 
 if [ $arg_test -eq 1 ]; then
   echo "引数エラーのチェック -------------------------------------------"
-  # number_of_philosophers
-  # time_to_die
-  # time_to_eat
-  # time_to_sleep
-  # [number_of_times_each_philosopher_must_eat]
 
   # 引数の個数：少ない
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
 
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo a
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
 
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo a a a
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
 
   # 引数の個数：多い
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo 1 1 1 1 1 1
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
 
   # 引数の値の種類：数値以外
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo a a a a
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
 
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo a a a a a
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
+
+  valgrind --leak-check=full --show-leak-kinds=all -q ./philo 1 a a a a
+  check_exit_status 1
+  valgrind --leak-check=full --show-leak-kinds=all -q ./philo 1 1 a a a
+  check_exit_status 1
+  valgrind --leak-check=full --show-leak-kinds=all -q ./philo 1 1 1 a a
+  check_exit_status 1
+  valgrind --leak-check=full --show-leak-kinds=all -q ./philo 1 1 1 1 a
+  check_exit_status 1
+
+  # 引数の値の種類：小数点
+  valgrind --leak-check=full --show-leak-kinds=all -q ./philo 0.321 0.321 0.321 0.321 0.321
+  check_exit_status 1
 
   # 引数の値の種類：INT_MAX以上
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo 2147483648 2147483648 2147483648 2147483648 2147483648
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
 
   # 引数の値の種類：負数
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo -1 -1 -1 -1 -1
-  if [ $? -ne 1 ]; then echo NG; fi
+  check_exit_status 1
 
   echo ""
 fi
 
+# メインの処理で、0のとき動くか。ft_atoiに不安あり
 if [ $main_test -eq 1 ]; then
   echo "メイン処理のチェック -------------------------------------------"
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo

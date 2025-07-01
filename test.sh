1a134594a098adb3339412fbbfc662313bc25978
M2U7BF <yhvh4321@gmail.com>
2025-07-01 09:24:01 +0900

    update

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ source "./${PROG_NAME}_test/test_util.sh"
 use_func_test=1
 norm_test=1
 func_test=0
-arg_test=0
-main_test=0
+arg_test=1
+main_test=1
 tester_test=0
 tester_test2=1
 
@@ -23,62 +23,58 @@ philo()
   expected_status=$1
   shift 1
 
-  echo "execution: ./philo $@"
+  echo "■[execution]: [./philo $@]"
   echo ""
 
-  echo "-- 通常実行 --"
-  ./philo "$@"
-  check_exit_status $expected_status
-  echo ""
-
-  echo "-- リークテスト --"
+  put_test_pattern "通常実行（リークテストあり）"
   # setarch $(uname -m) -R 
   valgrind --leak-check=full --show-leak-kinds=all -q ./philo "$@"
   check_exit_status $expected_status
   echo ""
 
-  echo "-- データ競合テスト --"
+  put_test_pattern "データ競合テスト"
   # setarch $(uname -m) -R 
   valgrind --tool=helgrind -q ./philo "$@"
   check_exit_status $expected_status
+  echo ""
   echo ""
 }
 
 make_mandatory
 
 if [ $use_func_test -eq 1 ]; then
-  echo "使用関数のチェック -------------------------------------------"
+  put_test_name "使用関数のチェック"
   nm -u ./philo | grep GLIBC | grep -v -E '__libc_start_main|memset|printf|malloc|free|write|usleep|gettimeofday|pthread_create|pthread_detach|pthread_join|pthread_mutex_init|pthread_mutex_destroy|pthread_mutex_lock|pthread_mutex_unlock'
   check_exit_status 1
   echo ""
 fi
 
 if [ $norm_test -eq 1 ]; then
-  echo "norminetteのチェック -------------------------------------------"
+  put_test_name "norminetteのチェック"
   find -maxdepth 1 -name "*.c" | xargs norminette
   check_exit_status 0
   echo ""
 fi
 
 if [ $func_test -eq 1 ]; then
-  echo "メイン以外の基本関数のテスト -------------------------------------------"
+  put_test_name "メイン以外の基本関数のテスト"
   cc -o ./func_test philo_test/test_main.c philo.a && ./func_test
   check_exit_status 0
   echo ""
 fi
 
 if [ $arg_test -eq 1 ]; then
-  echo "引数エラーのチェック -------------------------------------------"
+  put_test_name "引数エラーのチェック"
 
-  # 引数の個数：少ない
+  put_test_pattern "引数の個数：少ない"
   philo 1
   philo 1 a
   philo 1 a a a
 
-  # 引数の個数：多い
+  put_test_pattern "引数の個数：多い"
   philo 1 1 1 1 1 1 1
 
-  # 引数の値の種類：数値以外
+  put_test_pattern "引数の値の種類：数値以外"
   philo 1 a a a a
   philo 1 a a a a a
   philo 1 1 a a a a
@@ -86,20 +82,20 @@ if [ $arg_test -eq 1 ]; then
   philo 1 1 1 1 a a
   philo 1 1 1 1 1 a
 
-  # 引数の値の種類：小数点
+  put_test_pattern "引数の値の種類：小数点"
   philo 0 0.321 0.321 0.321 0.321 0.321
 
-  # 引数の値の種類：INT_MAX以上
+  put_test_pattern "引数の値の種類：INT_MAX以上"
   philo 1 2147483648 2147483648 2147483648 2147483648 2147483648
 
-  # 引数の値の種類：負数
+  put_test_pattern "引数の値の種類：負数"
   philo 1 -1 -1 -1 -1 -1
 
   echo ""
 fi
 
 if [ $main_test -eq 1 ]; then
-  echo "メイン処理のチェック -------------------------------------------"
+  put_test_name "メイン処理のチェック"
   # 実行時の構文：
   # ./philo \
   #   number_of_philosophers \
@@ -132,7 +128,7 @@ if [ $main_test -eq 1 ]; then
 fi
 
 if [ $tester_test -eq 1 ]; then
-  echo "テスターのテスト -------------------------------------------"
+  put_test_name "テスターのテスト"
   test -d philosophers_tester || git clone https://github.com/AntonioSebastiaoPedro/philosophers_tester.git
   philosophers_tester/philo_tester.sh -a
   philosophers_tester/philo_tester.sh -d
@@ -140,7 +136,7 @@ if [ $tester_test -eq 1 ]; then
 fi
 
 if [ $tester_test2 -eq 1 ]; then
-  echo "テスター2のテスト -------------------------------------------"
+  put_test_name "テスター2のテスト"
   test -d LazyPhilosophersTester || git clone https://github.com/MichelleJiam/LazyPhilosophersTester.git
   cd LazyPhilosophersTester
   ./test.sh ../philo
